b279b9c023eead3d458380faf45c49b9bcc18b99
biggetsskidonearth <niklasschaub311@gmx.de>
2026-02-01 17:00:11 +0100

    Create Settings.lua with game configuration options
    Add initial settings for aim, gun mods, ESP, and movement.

---
+++ Settings.lua
@@ -0,0 +1,22 @@
+-- Aim
+G_Enabled     = true
+G_FOV         = 1000
+G_Prediction  = 0.165
+G_TeamCheck   = true
+G_HitPart     = "Head"
+
+-- Gun mods
+G_NoRecoil    = true
+G_NoSpread    = true
+
+-- ESP
+G_ESP         = true
+G_HeadDots    = true
+G_Names       = true
+G_Distance    = true
+G_Health      = true
+
+-- Movement
+G_WalkSpeed   = 50
+G_JumpPower   = 100
+G_InfiniteJump = true
